de01565b06954195eeedd52cd5aec25ededd411c
Yuhan XU <56353466+claire-x@users.noreply.github.com>
2022-04-10 15:03:07 +0800

    Update match

--- sql/match_result.sql
+++ sql/match_result.sql
@@ -1,12 +1,16 @@
-/* ref: https://github.com/LI-YUXIN-Ryan-Garcia/CUPar-CSCI3100-Project.git */
+/* use database 'ubuy' in mysql and execute codes below*/
 DROP TABLE IF EXISTS `match_result`;
 CREATE TABLE `match_result` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `user_id1` varchar(45) NOT NULL,
-    `user_id2` varchar(45) NOT NULL,
+    `user_id1` varchar(45) NOT NULL,    /* the buyer's uid*/
+    `user_id2` varchar(45) NOT NULL,    /* the seller's uid*/
     `object` varchar(45) NOT NULL,
     `result` tinyint(1) NOT NULL,
-    `res1` tinyint(1) DEFAULT NULL,
+    `res1` tinyint(1) DEFAULT NULL,     
     `res2` tinyint(1) DEFAULT NULL,
+    `pid1` varchar(11) DEFAULT NULL,        /* the buyer's post id */
+    `pid2` varchar(11) DEFAULT NULL,        /* the seller's post id */
+    `remark1` varchar(200) NOT NULL,
+    `remark2` varchar(200) NOT NULL,
     PRIMARY KEY (`id`)
 );
